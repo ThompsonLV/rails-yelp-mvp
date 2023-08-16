6c182fd9088b133e0b421db086dbcd00455de984
Thomas Le Veo <thomasleveo@gmail.com>
2023-08-16 16:00:59 +0200

    Exercice done little css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts '------------------'
+puts 'Cleaning database'
+puts '------------------'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+puts '.......................'
+dishoom = { name: 'Dishoom', address: '12 rue du camenbert', category: 'chinese' }
+pizza_east = { name: 'Dominos Pizza', address: '198 avenue Pierre Edouard', category: 'italian' }
+pokawa = { name: 'Pokawa', address: '28 impasse Jean Moulin', category: 'japanese' }
+sushi_life = { name: 'Sushi for life', address: '9 avenue du Président', category: 'french' }
+bocuse = { name: 'Bocuse', address: '56A Shoreditch High St, London E1 6PQ', category: 'belgian' }
+
+[dishoom, pizza_east, pokawa, sushi_life, bocuse].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Creating #{restaurant.name}"
+end
+
+puts '------------------'
+puts 'Finished'
+puts '------------------'
